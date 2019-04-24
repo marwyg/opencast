86b5b05441ab3ac54a0b16ca7a9578e45303e42c
EdmundoDelGusto <edmund.gebert@gmail.com>
2019-04-24 17:15:50 +0200

    Commit before build

--- modules/admin-ui/src/main/webapp/scripts/modules/registration/controllers/registrationController.js
+++ modules/admin-ui/src/main/webapp/scripts/modules/registration/controllers/registrationController.js
@@ -22,250 +22,84 @@
 
 // Controller for creating a new event. This is a wizard, so it implements a state machine design pattern.
 angular.module('adminNg.controllers')
-.controller('RegistrationCtrl', ['$scope', '$timeout', 'Table', 'NewEventStates', 'NewEventResource', 'EVENT_TAB_CHANGE',
-  'Notifications', 'Modal',
-  function ($scope, $timeout, Table, NewEventStates, NewEventResource, EVENT_TAB_CHANGE, Notifications, Modal) {
-    $scope.state = 'information';
-    $scope.submitButtonText = "REGISTRATION.MODAL.CONTINUE";
-//    $scope.states = NewEventStates.get();
-//    // This is a hack, due to the fact that we need to read html from the server :(
-//    // Shall be banished ASAP
-//
-//    var metadata,
-//        accessController,
-//        // Reset all the wizard states
-//        resetStates = function () {
-//          angular.forEach($scope.states, function(state)  {
-//            if (angular.isDefined(state.stateController.reset)) {
-//              state.stateController.reset({resetDefaults: true});
-//            }
-//          });
-//        };
-//
-//    angular.forEach($scope.states, function (state) {
-//      if (state.stateController.isAccessState) {
-//        accessController = state.stateController;
-//      } else if (state.stateController.isMetadataState) {
-//        // MH-12854 get latest collections (series)
-//        state.stateController.reset({resetDefaults: true});
-//        metadata = state.stateController;
-//      }
-//    });
-//
-//    if (angular.isDefined(metadata) && angular.isDefined(accessController)) {
-//      accessController.setMetadata(metadata);
-//    }
-//
-//    $scope.$on(EVENT_TAB_CHANGE, function (event, args) {
-//      if (args.old !== args.current && args.old.stateController.isProcessingState) {
-//        args.old.stateController.save();
-//      }
-//
-//      if (args.current.stateController.isAccessState) {
-//        args.current.stateController.loadSeriesAcl();
-//      }
-//
-//      if (args.current.stateController.isSourceState) {
-//        if (!args.current.stateController.defaultsSet) {
-//          args.current.stateController.loadCaptureAgents();
-//          args.current.stateController.setDefaultsIfNeeded();
-//        }
-//        if (!args.current.stateController.hasAgents()) {
-//          args.current.stateController.ud.type = 'UPLOAD';
-//        }
-//      }
-//    });
-//
-//    $scope.submit = function () {
-//      var messageId, userdata = { metadata: []}, ace = [];
-//
-//      window.onbeforeunload = function (e) {
-//        var confirmationMessage = 'The file has not completed uploading.';
-//
-//        (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
-//        return confirmationMessage;                                //Webkit, Safari, Chrome etc.
-//      };
-//
-//      angular.forEach($scope.states, function (state) {
-//
-//        var o;
-//        if (state.stateController.isMetadataState) {
-//          for (o in state.stateController.ud) {
-//            if (state.stateController.ud.hasOwnProperty(o)) {
-//              userdata.metadata.push(state.stateController.ud[o]);
-//            }
-//          }
-//        } else if (state.stateController.isMetadataExtendedState) {
-//          for (o in state.stateController.ud) {
-//            if (state.stateController.ud.hasOwnProperty(o)) {
-//              userdata.metadata.push(state.stateController.ud[o]);
-//            }
-//          }
-//        } else if (state.stateController.isAccessState) {
-//          angular.forEach(state.stateController.ud.policies, function (policy) {
-//            if (angular.isDefined(policy.role)) {
-//              if (policy.read) {
-//                ace.push({
-//                  'action' : 'read',
-//                  'allow'  : policy.read,
-//                  'role'   : policy.role
-//                });
-//              }
-//
-//              if (policy.write) {
-//                ace.push({
-//                  'action' : 'write',
-//                  'allow'  : policy.write,
-//                  'role'   : policy.role
-//                });
-//              }
-//            }
-//
-//            angular.forEach(policy.actions.value, function(customAction){
-//              ace.push({
-//                'action' : customAction,
-//                'allow'  : true,
-//                'role'   : policy.role
-//              });
-//            });
-//          });
-//
-//          userdata.access = {
-//            acl: {
-//              ace: ace
-//            }
-//          };
-//        } else {
-//          userdata[state.name] = state.stateController.ud;
-//        }
-//      });
-//
-//      NewEventResource.save({}, userdata, function () {
-//        $timeout(function () {
-//          Table.fetch();
-//        }, 500);
-//
-//        Notifications.add('success', 'EVENTS_CREATED');
-//        Notifications.remove(messageId);
-//        resetStates();
-//        window.onbeforeunload = null;
-//      }, function () {
-//        Notifications.add('error', 'EVENTS_NOT_CREATED');
-//        Notifications.remove(messageId);
-//        resetStates();
-//        window.onbeforeunload = null;
-//      });
-//
-//      Modal.$scope.close();
-//      // add message that never disappears
-//      messageId = Notifications.add('success', 'EVENTS_UPLOAD_STARTED', 'global', -1);
-//    };
-//    
-    $scope.adopter = { 
-                       "allows_statistics": false,
-                       "allows_error_reports": false,
-                       "allows_tech_data": false, 
-                       "contact_me": false 
-                     };
-    
-    $scope.states = {
-        "information": {
-          "nextState": {
-            0: "skip",
-            1: "form"
-          },
-          "buttons": {
-            "submit": true,
-            "back": false,
-            "skip": true,
-            "close": false,
-            "submitButtonText": "REGISTRATION.MODAL.CONTINUE"
-          }
-        },
-        "form": {
-          "nextState": {
-            0: "information",
-            1: "save"
-          },
-          "buttons": {
-            "submit": true,
-            "back": true,
-            "skip": false,
-            "close": false,
-            "submitButtonText": "SUBMIT"
-          }
-        },
-        "save": {
-          "nextState": {
-            0: "close",
-            1: "error"
-          },
-          "buttons": {
-            "submit": false,
-            "back": false,
-            "skip": false,
-            "close": true,
-            "submitButtonText": null
-          }
-        },
-        "skip": {
-          "nextState": {
-            0: "close",
-            1: "error"
-          },
-          "buttons": {
-            "submit": false,
-            "back": false,
-            "skip": false,
-            "close": true,
-            "submitButtonText": null
-          }
-        }
-    }
-    
+.controller('RegistrationCtrl', ['$scope', '$timeout', 'Table', 'AdopterRegistrationStates', 'AdopterRegistrationResource', 'CountryResource',
+  'NewEventStates', 'NewEventResource', 'EVENT_TAB_CHANGE', 'Notifications', 'Modal',
+  function ($scope, $timeout, Table, AdopterRegistrationStates, AdopterRegistrationResource, CountryResource,
+      NewEventStates, NewEventResource, EVENT_TAB_CHANGE, Notifications, Modal) {
+    $scope.state = AdopterRegistrationStates.getInitialState($scope.$parent.resourceId);
+    $scope.states = AdopterRegistrationStates.get($scope.$parent.resourceId);
+    // TODO: CountryResource is in fact a service, but should be a resource. Look at countryResource.js for more information
+    $scope.countries = CountryResource.getCountries();
+
+    $scope.adopter = new AdopterRegistrationResource();
+    // TODO: check if already registered and fetch data. Set $scope.registered
+    // TODO: You need to implement the Endpoint first
+    //    AdopterRegistrationResource.get({}, function(response) {}});
+    $scope.registered = false;
+
     $scope.nextState = function (inputAction) {
-      // inputAction is 0 or 1
-      if($scope.state == null) {
-        // init
-        $scope.state = "information";
-      } else {
-        $scope.state = $scope.states[$scope.state]["nextState"][inputAction];
-        if($scope.state == "close"){
-          $scope.close();
-        } else if($scope.state == "save") {
-          $scope.save();
-        }
+      if($scope.state == 'form' && $scope.adopterRegistrationForm.$invalid
+          && (inputAction == 1 || inputAction == 3)) {
+        return;
+      }
+      $scope.state = $scope.states[$scope.state]["nextState"][inputAction];
+
+      if($scope.state == "close"){
+        $scope.close();
+      } else if($scope.state == "save") {
+        $scope.save();
+      } else if($scope.state == "update") {
+        $scope.updateProfile();
+      } else if($scope.state == "delete") {
+        $scope.deleteProfile();
       }
     }
+
     $scope.save = function () {
-      console.log($scope.adopter);
-      console.log($scope.adopterRegistrationForm.$valid)
-      $scope.state = "form"; //TODO: Delete this
+      // TODO: Perform real POST-Request to save adopter
+      // TODO: Endpoint needs to be implemented too
+      if($scope.adopterRegistrationForm.$valid) {
+        $timeout(function() {
+          $scope.nextState(0);
+        }, 2000);
+      }
+      //AdopterRegistrationResource.save($scope.adopter, function($response) {});
     }
 
-    
+    $scope.updateProfile = function () {
+      // TODO: Perform real PUT-Request to save adopter
+      // TODO: Endpoint needs to be implemented too
+      if($scope.adopterRegistrationForm.$valid) {
+        $timeout(function() {
+          var success = true;
+          if(success) {
+            Notifications.add('success', 'ADOPTER_PROFILE_UPDATED');
+          } else {
+            Notifications.add('error', 'ADOPTER_PROFILE_NOT_UPDATED');
+          }
+          $scope.nextState(0);
+        }, 2000);
+      }
+      //AdopterRegistrationResource.update($scope.adopter, function($response) {});
+    }
+
+    $scope.deleteProfile = function () {
+      // TODO: Perform real DELETE-Request to save adopter
+      // TODO: Endpoint needs to be implemented too
+      $timeout(function() {
+        console.log("delete profile")
+        var success = true;
+        if(success) {
+          Notifications.add('success', 'ADOPTER_PROFILE_DELETED');
+        } else {
+          Notifications.add('error', 'ADOPTER_PROFILE_NOT_DELETED');
+        }
+        $scope.nextState(0);
+      }, 2000);
+      //AdopterRegistrationResource.delete($scope.adopter, function($response) {});
+    }
+
     $scope.close = function () {
       Modal.$scope.close();
     };
-    
-    $scope.skip = function () {
-      $scope.state = 'skip';
-    };
-    
-    $scope.back = function() {
-      $scope.state = 'information';
-      $scope.submitButtonText = "REGISTRATION.MODAL.CONTINUE";
-    }
-    
-    $scope.submitAction = function() {
-      if($scope.state == "information") {
-        $scope.state = "form";
-        $scope.submitButtonText = "SAVE"
-        return;
-      } else if($scope.state == "form") {
-        $scope.state = "save";
-      }
-    };
-    
-    $scope.change = function (o) { console.log(o) }
   }]);
